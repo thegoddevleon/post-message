3f57079db4fc82936d41c86b4c468812f9d59724
Leon Scott <leon.s@sdbank.co>
2024-11-14 01:06:14 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useCallback } from "react";
+import "./App.css";
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    SDB: any;
+  }
+}
+
+function App() {
+  const onHandlePostMessage = useCallback(() => {
+    const windowSDB = window?.SDB;
+    if (windowSDB !== undefined) {
+      windowSDB?.postMessage({ key: "value" });
+    }
+  }, []);
+
+  return (
+    <>
+      <button onClick={onHandlePostMessage}>Post Message</button>
+    </>
+  );
+}
+
+export default App;
